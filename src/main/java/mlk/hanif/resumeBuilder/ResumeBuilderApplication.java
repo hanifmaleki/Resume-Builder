a708e5e4eef162a4e734f4c67a39b3e8cbea74b3
hanifmaleki <hanif_1362@yahoo.com>
2020-02-25 10:12:20 +0100

    RES-4 #comment correct the unused imports

--- src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
+++ src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
@@ -1,9 +1,6 @@
 package mlk.hanif.resumeBuilder;
 
-import javafx.stage.Stage;
 import mlk.hanif.resumeBuilder.model.Resume;
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,8 +10,6 @@ import java.net.URISyntaxException;
 @SpringBootApplication
 public class ResumeBuilderApplication {
 
-    private ApplicationContext ctx;
-
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(ResumeBuilderApplication.class, args);
         ContextMaker contextMaker = run.getBeanFactory().getBean(ContextMaker.class);
@@ -22,9 +17,7 @@ public class ResumeBuilderApplication {
         ReportGenerator reportGenerator = new ReportGenerator(resume, "tex", "test");
         try {
             reportGenerator.generateReport();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (WrongDirectoryException e) {
+        } catch (URISyntaxException | WrongDirectoryException e) {
             e.printStackTrace();
         }
     }
