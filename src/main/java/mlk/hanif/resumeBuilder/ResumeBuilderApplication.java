d66181d46bb8ba02ef84af9d55aaa3aedf88f6a3
hanifmaleki <hanif_1362@yahoo.com>
2020-02-29 18:53:26 +0100

    RES-10 #comment disable filewriting in order to solve Heroku problem

--- src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
+++ src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
@@ -12,13 +12,13 @@ public class ResumeBuilderApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(ResumeBuilderApplication.class, args);
-        ContextMaker contextMaker = run.getBeanFactory().getBean(ContextMaker.class);
+        /*ContextMaker contextMaker = run.getBeanFactory().getBean(ContextMaker.class);
         Resume resume = contextMaker.getContext();
         ReportGenerator reportGenerator = new ReportGenerator(resume, "tex", "test");
         try {
             reportGenerator.generateReport();
         } catch (URISyntaxException | WrongDirectoryException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
