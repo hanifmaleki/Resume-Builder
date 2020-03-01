83880545bf5ba6c7d493f359f1fa558c529ddfec
hanifmaleki <hanif_1362@yahoo.com>
2020-03-01 11:51:56 +0100

    RES-10 #comment change controller return value in order to solve Heroku problem

--- src/main/java/mlk/hanif/resumeBuilder/controller/MainController.java
+++ src/main/java/mlk/hanif/resumeBuilder/controller/MainController.java
@@ -1,11 +1,17 @@
 package mlk.hanif.resumeBuilder.controller;
 
+import mlk.hanif.resumeBuilder.ContextMaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private ContextMaker contextMaker;
+
     @GetMapping("/")
     public String getHomePage(Model model){
         String[] names = {"John", "Sebastian", "Mark", "Gerhard"};
