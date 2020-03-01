3a6d303eb3c4101b2cdb209dcb16a738b0e87654
hanifmaleki <hanif_1362@yahoo.com>
2020-03-01 11:09:45 +0100

    RES-10 #comment change controller return value in order to solve Heroku problem

--- src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
+++ src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
@@ -1,24 +1,13 @@
 package mlk.hanif.resumeBuilder;
 
-import mlk.hanif.resumeBuilder.model.Resume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class ResumeBuilderApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(ResumeBuilderApplication.class, args);
-        /*ContextMaker contextMaker = run.getBeanFactory().getBean(ContextMaker.class);
-        Resume resume = contextMaker.getContext();
-        ReportGenerator reportGenerator = new ReportGenerator(resume, "tex", "test");
-        try {
-            reportGenerator.generateReport();
-        } catch (URISyntaxException | WrongDirectoryException e) {
-            e.printStackTrace();
-        }*/
+       SpringApplication.run(ResumeBuilderApplication.class, args);
     }
 }
