0187c17a870d3e8fd7f9eb049323ca8d53f194d5
hanifmaleki <hanif_1362@yahoo.com>
2020-03-01 10:35:35 +0100

    RES-10 #comment change controller return value in order to solve Heroku problem

--- src/main/java/mlk/hanif/resumeBuilder/controller/PrsonalInfoController.java
+++ src/main/java/mlk/hanif/resumeBuilder/controller/PrsonalInfoController.java
@@ -1,6 +1,7 @@
 package mlk.hanif.resumeBuilder.controller;
 
 import mlk.hanif.resumeBuilder.ContextMaker;
+import mlk.hanif.resumeBuilder.model.PersonalInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PrsonalInfoController {
 
-    @Autowired
-    private ContextMaker contextMaker;
+//    @Autowired
+//    private ContextMaker contextMaker;
 
     @GetMapping("/")
     public String getPersonalInfo(Model model){
-        model.addAttribute("personalInfo", contextMaker.getContext().getPersonalInfo());
+        PersonalInfo personalInfo = new PersonalInfo(); //contextMaker.getContext().getPersonalInfo();
+        personalInfo.setName("Hanif");
+        personalInfo.setFamily("Maleki");
+        model.addAttribute("personalInfo", personalInfo);
         return "index";
     }
 
