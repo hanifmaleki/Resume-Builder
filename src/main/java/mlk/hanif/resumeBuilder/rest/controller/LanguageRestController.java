25d0f30cbf0589464cf99a78a9c2f233ec7dc0d5
hmaleki <hanif.maleki@bearingpoint.com>
2020-03-06 13:38:09 +0100

    RES-13 #comment Auto Generation compability #done #time 2h

--- src/main/java/mlk/hanif/resumeBuilder/rest/controller/LanguageRestController.java
+++ src/main/java/mlk/hanif/resumeBuilder/rest/controller/LanguageRestController.java
@@ -2,14 +2,17 @@ package mlk.hanif.resumeBuilder.rest.controller;
 
 import mlk.hanif.resumeBuilder.ContextMaker;
 import mlk.hanif.resumeBuilder.model.Language;
+import mlk.hanif.resumeBuilder.restApi.api.LanguageApi;
+import mlk.hanif.resumeBuilder.restApi.model.LanguageResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class LanguageRestController {
+public class LanguageRestController implements LanguageApi {
 
      @Autowired
      private ContextMaker contextMaker;
@@ -17,5 +20,10 @@ public class LanguageRestController {
     @GetMapping(value = "/languages")
     public List<Language> getLanguages(){
         return contextMaker.getContext().getLanguages();
+    }
+
+    @Override
+    public ResponseEntity<List<LanguageResource>> languageGet() {
+        return null;
     }
 }
