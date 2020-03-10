9f7acef3dbd489de4fc71e1b10b6fcb377630fa4
hmaleki <hanif.maleki@bearingpoint.com>
2020-03-10 16:01:16 +0100

    RES-16 #time 2h #comment Add ControllerAdvice class in order to support common vars in models #in-progress

---
+++ src/main/java/mlk/hanif/resumeBuilder/controller/GlobalControllerAdvice.java
@@ -0,0 +1,36 @@
+package mlk.hanif.resumeBuilder.controller;
+
+import mlk.hanif.resumeBuilder.model.Certificate;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    //TODO study and add @InitBinder
+    //TODO study and add @ExceptionHandler
+    //TODO https://www.concretepage.com/spring/spring-mvc/spring-mvc-controlleradvice-annotation-example
+
+    Map<String, String> pages = new HashMap<String, String>(){
+        {
+            put("home", "personalInfo");
+            put("education", "education");
+            put("skill", "skill");
+            put("experience", "experience");
+            put("language", "language");
+            put("academicProjects", "academicProjects");
+            put("certificates", "certificates");
+        }
+    };
+
+    @ModelAttribute
+    public void handleRequest(HttpServletRequest request, Model model){
+        model.addAttribute("pages", pages);
+    }
+
+}
