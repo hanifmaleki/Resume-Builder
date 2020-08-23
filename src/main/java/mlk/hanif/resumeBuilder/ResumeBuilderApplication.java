ddb8ec5310a2ff4b7051124a0be1bedf5a1f4a77
hanifmaleki <hanif_1362@yahoo.com>
2020-08-23 17:14:25 +0200

    RES-31 #comment Add tabs to experiencesand cards #time 1h #done

--- src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
+++ src/main/java/mlk/hanif/resumeBuilder/ResumeBuilderApplication.java
@@ -1,13 +1,28 @@
 package mlk.hanif.resumeBuilder;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class ResumeBuilderApplication {
 
     public static void main(String[] args) {
        SpringApplication.run(ResumeBuilderApplication.class, args);
+    }
+
+    @Bean
+    public ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.addDialect(new Java8TimeDialect());
+        engine.setTemplateResolver(templateResolver);
+        return engine;
     }
 }
