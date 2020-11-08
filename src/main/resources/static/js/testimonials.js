3400169ae0e1fb21a70d621c645a7fa4b81b28c0
Hanif Maleki <hanif_1362@yahoo.com>
2020-11-08 22:57:39 +0100

    RES-31 Add experience and akill parts #time 4h #done

---
+++ src/main/resources/static/js/testimonials.js
@@ -0,0 +1,29 @@
+const slider = tns({
+    container: '.testimonial-carousel',
+    loop: true,
+    items: 1,
+    /*responsive: {
+	    0:{
+	            items:1,
+        },
+        768:{
+            items:2,
+        },
+        1200: {
+	        items:3,
+        }
+
+	},*/
+    slideBy: 'page',
+    nav: true,
+    autoplay: false,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    lazyload: true,
+    gutter: 30,
+    navPosition: 'bottom',
+    mouseDrag: true,
+    controls: false,
+    speed: 300,
+
+});
